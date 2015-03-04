148dd4468a1fe5d9c9035db7abf4897bf1c5b83d
Jake Drahos <jake@laptop.site>
2015-03-04 02:14:21 -0600

    Fixed database
    Changes to secure login still implemented.

--- isucdc2/settings.py
+++ isucdc2/settings.py
@@ -56,8 +56,11 @@ WSGI_APPLICATION = 'isucdc2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME' : os.path.join(BASE_DIR, 'db.sqlite3')    
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cdc',
+        'USER': 'root',
+        'PASSWORD': 'cdc',
+        'HOST': '',
     }
 }
 
